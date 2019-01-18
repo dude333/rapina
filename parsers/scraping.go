883e24da3f6216eda7322603746dc8f640e5ed3c
Adriano P <dev@dude333.com>
2019-01-18 02:25:09 -0200

    Cleanup

--- parsers/scraping.go
+++ parsers/scraping.go
@@ -8,70 +8,20 @@ import (
 	"github.com/gocolly/colly"
 )
 
-func companies(url string) {
-	c := colly.NewCollector(
-		// Restrict crawling to specific domains
-		// colly.AllowedDomains("bvmf.bmfbovespa.com.br"),
-		// Allow visiting the same page multiple times
-		colly.AllowURLRevisit(),
-		// Allow crawling to be done in parallel / async
-		colly.Async(false),
-		colly.CacheDir(".data/cache"),
-	)
-
-	// Find and visit all links
-	c.OnHTML("tr", func(e *colly.HTMLElement) {
-		// if e.Attr("class") != "GridRow_SiteBmfBovespa GridBovespaItemStyle" {
-		// 	return
-		// }
-
-		e.ForEachWithBreak("a", func(_ int, elem *colly.HTMLElement) bool {
-			if strings.Contains(elem.Attr("href"), "ResumoEmpresaPrincipal.aspx") {
-				fmt.Println("              -", elem.Text)
-			}
-			return false // get only the 1st elem
-		})
-
-	})
-
-	// c.OnResponse(func(r *colly.Response) {
-	// 	fmt.Println(string(r.Body))
-	// })
-
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Visiting", r.URL)
-	// })
-
-	c.Visit(url)
-	// c.Visit("http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/BuscaEmpresaListada.aspx?segmento=Bancos")
-	// c.Visit("http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/BuscaEmpresaListada.aspx?segmento=Explora%c3%a7%c3%a3o+de+Im%c3%b3veis")
-}
-
+//
+// Sectors grab data from B3 website and prints out to a yaml file
+// with all companies grouped by sector, subsector, segment
+//
 func Sectors() {
 	c := colly.NewCollector(
 		// Restrict crawling to specific domains
 		// colly.AllowedDomains("bvmf.bmfbovespa.com.br"),
-		// Allow visiting the same page multiple times
 		colly.AllowURLRevisit(),
-		// Allow crawling to be done in parallel / async
 		colly.Async(false),
 		colly.CacheDir(".data/cache"),
 	)
 
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		// e.Request.Visit(e.Attr("href"))
-		// fmt.Printf("> %v\n", e.Attr("href"))
-	})
-
 	c.OnHTML("tr", func(e *colly.HTMLElement) {
-		// if e.Attr("class") != "GridRow_SiteBmfBovespa GridBovespaItemStyle" {
-		// 	return
-		// }
-
-		// h, _ := e.DOM.Find("td").Html()
-		// fmt.Printf("=> %#v \n", h)
-
 		var sector string
 		var subsectors []string
 		c := 0
@@ -110,23 +60,38 @@ func Sectors() {
 				}
 			})
 		})
-
-		// s := col[2]
-		// if len(col[2]) > 3 {
-		// 	s = s[2:]
-		// }
-		// fmt.Printf("|=> %s \n |==> %s \n  |====> %s\n", col[0], col[1], s)
-
 	})
-
-	// c.OnResponse(func(r *colly.Response) {
-	// fmt.Println(string(r.Body))
-	// })
-
-	// c.OnRequest(func(r *colly.Request) {
-	// fmt.Println("Visiting", r.URL)
-	// })
 
 	fmt.Println("Setores:")
 	c.Visit("http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/BuscaEmpresaListada.aspx?opcao=1&indiceAba=1&Idioma=pt-br")
+}
+
+//
+// companies lists all companies in the same sector/subsector/segment
+//
+func companies(url string) {
+	c := colly.NewCollector(
+		// Restrict crawling to specific domains
+		// colly.AllowedDomains("bvmf.bmfbovespa.com.br"),
+		colly.AllowURLRevisit(),
+		colly.Async(false),
+		colly.CacheDir(".data/cache"),
+	)
+
+	// Find and visit all links
+	c.OnHTML("tr", func(e *colly.HTMLElement) {
+		// if e.Attr("class") != "GridRow_SiteBmfBovespa GridBovespaItemStyle" {
+		// 	return
+		// }
+
+		e.ForEachWithBreak("a", func(_ int, elem *colly.HTMLElement) bool {
+			if strings.Contains(elem.Attr("href"), "ResumoEmpresaPrincipal.aspx") {
+				fmt.Println("              -", elem.Text)
+			}
+			return false // get only the 1st elem
+		})
+
+	})
+
+	c.Visit(url)
 }
