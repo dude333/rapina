8899925fcc3b433368d8a81f7b9ac8788ab78f3d
Adriano P <dev@dude333.com>
2019-01-29 15:55:51 -0200

    Fix test

--- parsers/sectors_test.go
+++ parsers/sectors_test.go
@@ -1,11 +1,16 @@
 package parsers
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func TestFromSector(t *testing.T) {
-	s, _ := FromSector("GRENDENE S.A.", "../cli/setores.yml")
+	const filename = "/tmp/test_sectors.yml"
+
+	createYaml(filename)
+	s, _ := FromSector("GRENDENE S.A.", filename)
 	expected := [...]string{"ALPARGATAS S.A.", "CAMBUCI S.A.", "GRENDENE S.A.", "VULCABRAS/AZALEIA S.A."}
 	if len(s) != 4 {
 		t.Errorf("\n- Expected:  %v\n- Got:       %v", expected, s)
@@ -16,4 +21,35 @@ func TestFromSector(t *testing.T) {
 	if arr != expected {
 		t.Errorf("\n- Expected:  %v\n- Got:       %v", expected, s)
 	}
+
+	os.Remove(filename)
+}
+
+func createYaml(filename string) {
+	yaml := []byte(
+		`Setores:
+- Setor: Bens Industriais
+  Subsetores:
+    - Subsetor: Comércio
+      Segmentos:
+        - Segmento: Material de Transporte
+          Empresas:
+            - MINASMAQUINAS S.A.
+            - WLM PART. E COMÉRCIO DE MÁQUINAS E VEÍCULOS S.A.
+- Setor: Consumo Cíclico
+  Subsetores:
+    - Subsetor: Tecidos. Vestuário e Calçados
+      Segmentos:
+        - Segmento: Acessórios
+          Empresas:
+          - MUNDIAL S.A. - PRODUTOS DE CONSUMO
+          - TECHNOS S.A.
+        - Segmento: Calçados
+          Empresas:
+            - ALPARGATAS S.A.
+            - CAMBUCI S.A.
+            - GRENDENE S.A.
+            - VULCABRAS/AZALEIA S.A.`)
+
+	ioutil.WriteFile(filename, yaml, 0644)
 }
